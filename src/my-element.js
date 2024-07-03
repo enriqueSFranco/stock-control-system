22cd066a9bcd71bd409a61c57b9daa45659edd56
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-07-03 12:52:12 -0600

    clean project

--- src/my-element.js
+++ src/my-element.js
@@ -9,78 +9,25 @@ import viteLogo from '/vite.svg'
  * @csspart button - The button
  */
 export class MyElement extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * Copy for the read the docs hint.
-       */
-      docsHint: { type: String },
-
-      /**
-       * The number of times the button has been clicked.
-       */
-      count: { type: Number },
-    }
-  }
 
   constructor() {
     super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
   }
 
-  render() {
+  render () {
     return html`
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
       </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
     `
   }
 
-  _onClick() {
-    this.count++
-  }
-
-  static get styles() {
+  static get styles () {
     return css`
       :host {
         max-width: 1280px;
         margin: 0 auto;
         padding: 2rem;
         text-align: center;
-      }
-
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
       }
 
       a {
@@ -91,12 +38,7 @@ export class MyElement extends LitElement {
       a:hover {
         color: #535bf2;
       }
-
-      ::slotted(h1) {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
+        
       button {
         border-radius: 8px;
         border: 1px solid transparent;
