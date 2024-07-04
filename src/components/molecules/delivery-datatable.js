754421309cf64656facdf0524032fa8ad4d3fb21
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-07-04 13:22:39 -0600

    feat: componetizacion de tableRow y renderCell

--- src/components/molecules/delivery-datatable.js
+++ src/components/molecules/delivery-datatable.js
@@ -2,7 +2,7 @@ import { LitElement, css, html } from 'lit'
 
 import '../atoms/table-row.js'
 import '../atoms/table-head.js'
-import { TRUCKS } from '../../shared/constants.d.js'
+import { TRUCKS, TABLE_HEAD } from '../../shared/constants.d.js'
 
 export class DeliveryDatatable extends LitElement {
   static styles = css`
@@ -11,8 +11,31 @@ export class DeliveryDatatable extends LitElement {
       width: 100%;
     }
 
+    table th,
+    table td {
+      padding: 10px;
+      text-align: left;
+    }
+
+    table th {
+      background-color: #021ab3;
+      color: #fff;
+    }
+
+    table tbody tr:hover {
+      background-color: #53535362;
+    }
+
     tr {
       border-bottom: 1px solid #ddd;
+    }
+
+    .status-pending {
+      color: #f77f00;
+    }
+
+    .status-delivered {
+      color: #4caf50;
     }
   `
   static properties = {
@@ -27,14 +50,42 @@ export class DeliveryDatatable extends LitElement {
   render () {
     return html`
       <table>
-        <table-head></table-head>
+        <thead>
+          <tr>
+            ${TABLE_HEAD.map((tableHeadText) => html`<th>${tableHeadText}</th>`)}
+          </tr>
+        </thead>
         <tbody>
-          ${this.trucks.map((truck) => html`
-              <table-row .truck="${truck}"></table-row>
-            `)}
+          ${this.trucks.map((truck) => this._renderTableRow(truck))}
         </tbody>
       </table>
     `
+  }
+
+  // TODO: Pasar a un componente
+  _renderTableRow (row) {
+    return html`
+      <tr>
+        ${Object.keys(row).map((key) => this._renderCell(key, row[key]))}
+      </tr>
+    `
+  }
+
+  // TODO: Pasar a un componente
+  _renderCell (key, value) {
+    if (key === 'estado') {
+      const statusColor = this._getStatusColor(value)
+      // TODO: Implementarlo con el componente <my-badge></my-badge>
+      return html`<td class="${statusColor}">${value}</td>`
+    } else {
+      return html`<td>${value}</td>`
+    }
+  }
+
+  _getStatusColor (status) {
+    return status === 'entregado'
+      ? 'status-delivered'
+      : 'status-pending'
   }
 }
 
