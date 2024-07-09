63bb1dcc632a39900a48549dd9d2d6689ac3d92b
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-07-09 10:36:47 -0600

    feat(): se agrego el componente delivery-datatable

--- src/pages/RouteTracking.js
+++ src/pages/RouteTracking.js
@@ -1,24 +1,25 @@
 /**
  * Filename: RouteTracking.js
- * Author: 
+ * Author: Enrique SFranco and Viridiana Olvera Mendoza
  * Description: Entry point for route tracking feature
  */
 
-import { LitElement, html } from "lit";
-import "../components/templates/BBVATemplate"
+import { LitElement, html } from 'lit'
+import '../components/templates/BBVATemplate'
+import '../components/molecules/delivery-datatable.js'
 
-class RouteTracking extends LitElement{
-    static get is(){
-        return 'route-tracking';
-    }
+class RouteTracking extends LitElement {
+  static get is () {
+    return 'route-tracking'
+  }
 
-    render(){
-        return html`
-            <bbva-template>
-                <div>RouteTracking</div>
-            </bbva-template>
-        `;
-    }
+  render () {
+    return html`
+      <bbva-template>
+        <delivery-datatable></delivery-datatable>
+      </bbva-template>
+    `
+  }
 }
 
-window.customElements.define(RouteTracking.is, RouteTracking);
+window.customElements.define(RouteTracking.is, RouteTracking)
